82fc6bab29e19479e496a44be9fc56c6223395c0
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2024-12-09 21:32:06 -0400

    added scripts, tests and can now deploy, flip mood etc successfully

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -5,6 +5,9 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    // errors
+    error MoodNft__CantFlipMoodIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvgImageUri;
     string private s_happySvgImageUri;
@@ -26,6 +29,19 @@ contract MoodNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // Only want the owner to be able to flip the mood
+        // require(ownerOf(tokenId) == msg.sender, "Only the owner can flip the mood");
+        if (!_isAuthorized(_ownerOf(tokenId), msg.sender, tokenId)) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
